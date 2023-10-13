ab002d3fc4394b2d4706885cb0b3e420f2d4be06
marcos-schlautmann <marcos27.bonfim@gmail.com>
2023-10-13 19:34:35 -0300

    Configurando, criando e fazendo alguns ajuste no código de uma aws lambda.

---
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -0,0 +1,27 @@
+package helloworld;
+
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+        try {
+            return response.withStatusCode(200).withBody("{ \"message\": \"Hello World!\"}");
+        } catch (Exception ex) {
+            return response.withStatusCode(500).withBody("{ \"message\": \"Hello World not working!\"}");
+        }
+    }
+}
